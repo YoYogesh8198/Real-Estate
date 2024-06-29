c3dfef79a4740db578e2d095592ef6b8c20dd583
Yog esh <jangiry035@gmail.com>
2024-06-29 15:03:36 +0530

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React from 'react';
+import './Header.css';
+import logo from "../../../public/logo.png"
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+     return (
+          <section className='h-wrapper'>
+               <div className="flexCenter paddings innerWidth h-container">
+                    <img src={logo} alt="logo" width={100} />
+
+
+                    <div className="h-menu">
+                         <a href="">
+                              Residencies
+                         </a>
+                         <a href="">
+                              Our Value
+                         </a>
+                         <a href="">
+                              Contact
+                         </a>
+                         <a href="">
+                              Get Started
+                         </a>
+                         <button>
+                              <a href="">
+                                   Contact
+                              </a>
+                         </button>
+                    </div>
+               </div>
+          </section>
+     )
 }
 
 export default Header
